53051cb21aff4f8a83e25cb1a46841e24223612d
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-09-20 18:36:54 -0400

    Update Lírico AI update dialog

--- buzz/widgets/update_dialog.py
+++ buzz/widgets/update_dialog.py
@@ -59,7 +59,7 @@ class UpdateDialog(QDialog):
 
         #header
         header_label = QLabel(
-            _("A new version of Transcript is available!")
+            _("A new version of Lírico AI is available!")
         )
 
         header_label.setStyleSheet("font-size: 16px; font-weight: bold;")
